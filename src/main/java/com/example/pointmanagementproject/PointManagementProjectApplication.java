ef684d16eb86a43ec44f59cdaaca0240170a25d6
An T.G <boltcarrier@naver.com>
2023-09-25 20:16:28 +0900

    init project

---
+++ src/main/java/com/example/pointmanagementproject/PointManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.pointmanagementproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointManagementProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PointManagementProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pointmanagementproject/PointManagementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pointmanagementproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointManagementProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
